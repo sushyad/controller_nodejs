16c1f9ee23faa2410a787fd0233ac9a3259084b3
Susheel Yadav <sushyad@gmail.com>
2014-10-04 18:54:22 +0000

    Added support for mqtt bases command queue

--- controller.js
+++ controller.js
@@ -21,7 +21,37 @@ var port = process.env.PORT || 8080; 		// set our port
 var router = express.Router(); 				// get an instance of the express Router
 
 var serialport = require("serialport");
-var serial = new serialport.SerialPort("/dev/ttyUSB1", { baudrate : 115200, parser: serialport.parsers.readline("\n") });
+var serial = new serialport.SerialPort("/dev/ttyUSB0", { baudrate : 9600, parser: serialport.parsers.readline("\n") });
+
+mqttClient.subscribe('wsn/+/command');
+
+mqttClient.on('message', function(topic, message) {
+  //sample message: Switch21_BTN0:ON
+
+  var topicTokens = topic.split('/');
+  var item = topicTokens[1];
+  console.log(item + ':' + message);
+  var indexOfBtn = item.indexOf('_BTN');
+  var indexOfSwitch = item.indexOf('Switch');
+  if (indexOfSwitch == 0) {
+   if (indexOfBtn > 0) {
+      var switchId = item.substring(6, indexOfBtn);
+      var buttonId = item.substring(indexOfBtn + 4);
+      var buttonState = '0';
+      if (message == 'ON') buttonState = '1';
+      console.log('Received command for light switch ' + switchId + '-' + buttonId + ':' +  message);
+      serial.write('LSWITCH' + switchId + ':' + buttonId + ':' + buttonState + '\r');
+    } else {
+      var switchId = item.substring(6).toUpperCase();
+      console.log('Received command for switch ' + switchId + ': ' + message);
+      if (switchId == 'PROJECTOR') {
+        serial.write(switchId + message + '\r');
+      } else {
+        serial.write('SWITCH' + switchId + message + '\r');
+      }
+    }
+  }
+});
 
 serial.on("data", function (data) {
   var status = '';
@@ -32,9 +62,34 @@ serial.on("data", function (data) {
   if (data.indexOf(' UNKNOWN ') != -1) status = 'Unknown';
 
   if (data.length > 0) {
-    if (data.indexOf('Pulsecount') > -1) {
-      //console.log('Will publish pulse count data: ' + data);
-      mqttClient.publish('wsn/electric_meter', data, function() {
+    console.log('Data: ' + data);
+
+    var indexOfPulseCount = data.indexOf('Pulsecount');
+    var indexOfBtn = data.indexOf('BTN');
+    if (indexOfPulseCount > -1) {
+      data = data.trim();
+      var indexOfPower =  data.indexOf('Power');
+      var pulseCount = data.substring(indexOfPulseCount + 12, indexOfPower - 1);
+      var power = data.substring(indexOfPower + 7);
+
+      //console.log('Will publish pulse count: "' + pulseCount + '"');
+      //console.log('Will publish power: "' + power + '"');
+
+      mqttClient.publish('wsn/electric_meter/pulseCount', pulseCount, function() {
+        //console.log("Message has been published");
+      });
+
+      mqttClient.publish('wsn/electric_meter/power', power, function() {
+        //console.log("Message has been published");
+      });
+    } else if (indexOfBtn > 0 && data.indexOf('Command') < 0) {
+      data = data.trim();
+      var switchId = data.substring(data.indexOf('[') + 1, data.indexOf(']'));
+      var indexOfColon = data.indexOf(':');
+      var buttonId = data.substring(indexOfBtn + 3, indexOfColon);
+      var buttonState = data.substring(indexOfColon + 1);
+      console.log('Will publish to topic ' + 'wsn/Switch' + switchId + '_BTN' + buttonId + '/state', buttonState == '0' ? 'OFF' : 'ON');
+      mqttClient.publish('wsn/Switch' + switchId + '_BTN' + buttonId + '/state', buttonState == '0' ? 'OFF' : 'ON', function() {
         //console.log("Message has been published");
       });
     }
@@ -44,6 +99,12 @@ serial.on("data", function (data) {
 router.put('/switches/:switchID?', function(req, res) {
 	console.log('Received command for switch ' + req.params.switchID + ': ' + req.body.state);
 	serial.write('SWITCH' + req.params.switchID + req.body.state + '\r');
+	res.json({ message: 'sent the command to switch ' + req.params.switchID });
+});
+
+router.put('/lightSwitches/:switchID?/buttons/:buttonID?', function(req, res) {
+	console.log('Received command for light switch ' + req.params.switchID + ': ' + req.body.state);
+	serial.write('LSWITCH' + req.params.switchID + ':' + req.params.buttonID + ':' + req.body.state + '\r');
 	res.json({ message: 'sent the command to switch ' + req.params.switchID });
 });
 
